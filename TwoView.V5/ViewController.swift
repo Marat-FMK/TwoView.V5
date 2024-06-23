33c71ea3f7974a63e697c2c9a900237f267fc294
MaratXcode <kosjachok8@yandex.ru>
2024-06-24 00:37:59 +0300

    Initial Commit

---
+++ TwoView.V5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoView.V5
+//
+//  Created by Marat Fakhrizhanov on 24.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
